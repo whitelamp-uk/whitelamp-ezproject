560d2369896e57318d130521c77c2333b6c6dfb1
Mark Page <mark.page@burdenandburden.co.uk>
2020-01-02 17:44:33 +0000

    First commit

---
+++ _hpapi/EzProject.routines.sql
@@ -0,0 +1,241 @@
+-- Adminer 4.7.5 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `ezpApp`$$
+CREATE PROCEDURE `ezpApp`(
+  IN        `app` VARCHAR(64) CHARSET ascii
+)
+BEGIN
+  SELECT
+    SUM(`ezp_estimate`.`hours`) AS `Total_Estimated`
+   ,ROUND(SUM(`ezp_estimate`.`hours`*(100-`ezp_component`.`progress_pct`)/100),1) AS `Total_Remaining`
+  FROM `ezp_component`
+  JOIN `ezp_difficulty`
+    ON `ezp_difficulty`.`difficulty`=`ezp_component`.`difficulty`
+  JOIN `ezp_estimate`
+    ON `ezp_estimate`.`difficulty`=`ezp_difficulty`.`difficulty`
+   AND `ezp_estimate`.`devtype`=`ezp_component`.`devtype`
+  WHERE `ezp_component`.`package` LIKE CONCAT(app,'-%')
+  ;
+  SELECT
+    `ezp_component`.`vendor`
+   ,`ezp_component`.`package`
+   ,`ezp_component`.`handle`
+   ,`ezp_component`.`devtype`
+   ,`ezp_difficulty`.`difficulty` AS `Difficulty_Level`
+   ,`ezp_estimate`.`hours` AS `Estimated_Hours`
+   ,ROUND(`ezp_estimate`.`hours`*((100-`ezp_component`.`progress_pct`)/100),1) AS `Hours_Remaining`
+  FROM `ezp_component`
+  JOIN `ezp_difficulty`
+    ON `ezp_difficulty`.`difficulty`=`ezp_component`.`difficulty`
+  JOIN `ezp_estimate`
+    ON `ezp_estimate`.`difficulty`=`ezp_difficulty`.`difficulty`
+   AND `ezp_estimate`.`devtype`=`ezp_component`.`devtype`
+  WHERE `ezp_component`.`package` LIKE CONCAT(app,'-%')
+  ORDER BY `ezp_component`.`devtype`,`ezp_component`.`package`,`ezp_component`.`handle`
+  ;
+END$$
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `ezpTimesheet`$$
+CREATE PROCEDURE `ezpTimesheet`(
+    IN `weekEndedSundayOrEmptyForAll` date
+   ,IN `developerOrEmptyForAll` varchar(64) charset ascii
+   ,IN `projectOrEmptyForAll` varchar(64) charset ascii
+)
+BEGIN
+  SELECT
+    *
+   ,DAYNAME(`day`) AS `dow`
+   ,`day`
+   ,`hours`
+   ,`developer`
+   ,`project`
+   ,`comment`
+  FROM `ezp_timesheet`
+  WHERE 1
+    AND (
+         weekEndedSundayOrEmptyForAll IS NULL
+      OR weekEndedSundayOrEmptyForAll=''
+      OR weekEndedSundayOrEmptyForAll='0000-00-00'
+      OR (
+           DAYOFWEEK(weekEndedSundayOrEmptyForAll)=1
+       AND `day`>DATE_SUB(weekEndedSundayOrEmptyForAll,INTERVAL 1 WEEK)
+       AND `day`<=weekEndedSundayOrEmptyForAll
+      )
+    )
+    AND (
+         projectOrEmptyForAll IS NULL
+      OR projectOrEmptyForAll=''
+      OR `project`=projectOrEmptyForAll
+    )
+    AND (
+        developerOrEmptyForAll IS NULL
+     OR developerOrEmptyForAll=''
+     OR `developer`=developerOrEmptyForAll
+    )
+  ORDER BY `day`,`developer`,`id`
+  ;
+END$$
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `ezpJobs`$$
+CREATE PROCEDURE `ezpJobs`(
+  IN        `developer` VARCHAR(64) CHARSET ascii
+)
+BEGIN
+  SELECT
+    `ezp_component`.`vendor`
+   ,`ezp_component`.`package`
+   ,`ezp_component`.`handle`
+   ,`ezp_component`.`devtype`
+   ,`ezp_difficulty`.`difficulty` AS `Difficulty_Level`
+   ,`ezp_estimate`.`hours` AS `Estimated_Hours`
+   ,ROUND(`ezp_estimate`.`hours`*((100-`ezp_component`.`progress_pct`)/100),1) AS `Hours_Remaining`
+   ,`ezp_component`.`required_by`
+   ,`ezp_component`.`notes`
+  FROM `ezp_component`
+  JOIN `ezp_difficulty`
+    ON `ezp_difficulty`.`difficulty`=`ezp_component`.`difficulty`
+  JOIN `ezp_estimate`
+    ON `ezp_estimate`.`difficulty`=`ezp_difficulty`.`difficulty`
+   AND `ezp_estimate`.`devtype`=`ezp_component`.`devtype`
+  WHERE `ezp_component`.`developer`=developer
+    AND `ezp_component`.`progress_pct`<100
+  ORDER BY `ezp_component`.`required_by` IS NULL,`ezp_component`.`required_by`
+  ;
+END$$
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `ezpPackage`$$
+CREATE PROCEDURE `ezpPackage`(
+  IN        `pkg` VARCHAR(64) CHARSET ascii
+)
+BEGIN
+  SELECT
+    SUM(`ezp_estimate`.`hours`) AS `Total_Estimated`
+   ,ROUND(SUM(`ezp_estimate`.`hours`*(100-`ezp_component`.`progress_pct`)/100),1) AS `Total_Remaining`
+  FROM `ezp_component`
+  JOIN `ezp_difficulty`
+    ON `ezp_difficulty`.`difficulty`=`ezp_component`.`difficulty`
+  JOIN `ezp_estimate`
+    ON `ezp_estimate`.`difficulty`=`ezp_difficulty`.`difficulty`
+   AND `ezp_estimate`.`devtype`=`ezp_component`.`devtype`
+  WHERE `ezp_component`.`package`=pkg
+  ;
+  SELECT
+    `ezp_component`.`vendor`
+   ,`ezp_component`.`package`
+   ,`ezp_component`.`handle`
+   ,`ezp_component`.`devtype`
+   ,`ezp_difficulty`.`difficulty` AS `Difficulty_Level`
+   ,`ezp_estimate`.`hours` AS `Estimated_Hours`
+   ,ROUND(`ezp_estimate`.`hours`*((100-`ezp_component`.`progress_pct`)/100),1) AS `Hours_Remaining`
+   ,`ezp_component`.`notes`
+  FROM `ezp_component`
+  JOIN `ezp_difficulty`
+    ON `ezp_difficulty`.`difficulty`=`ezp_component`.`difficulty`
+  JOIN `ezp_estimate`
+    ON `ezp_estimate`.`difficulty`=`ezp_difficulty`.`difficulty`
+   AND `ezp_estimate`.`devtype`=`ezp_component`.`devtype`
+  WHERE `ezp_component`.`package`=pkg
+  ORDER BY `ezp_component`.`devtype`,`ezp_component`.`handle`
+  ;
+END$$
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `ezpSummary`$$
+CREATE PROCEDURE `ezpSummary`(
+)
+BEGIN
+  SELECT
+    SUM(`ezp_estimate`.`hours`) AS `Total_Estimated`
+   ,ROUND(SUM(`ezp_estimate`.`hours`*(100-`ezp_component`.`progress_pct`)/100),1) AS `Total_Remaining`
+  FROM `ezp_component`
+  JOIN `ezp_difficulty`
+    ON `ezp_difficulty`.`difficulty`=`ezp_component`.`difficulty`
+  JOIN `ezp_estimate`
+    ON `ezp_estimate`.`difficulty`=`ezp_difficulty`.`difficulty`
+   AND `ezp_estimate`.`devtype`=`ezp_component`.`devtype`
+  WHERE 1
+  ;
+  SELECT
+    `ezp_component`.`vendor`
+   ,`ezp_component`.`package`
+   ,SUM(`ezp_estimate`.`hours`) AS `Estimated_Hours`
+   ,ROUND(SUM(`ezp_estimate`.`hours`*((100-`ezp_component`.`progress_pct`)/100)),1) AS `Hours_Remaining`
+  FROM `ezp_component`
+  JOIN `ezp_difficulty`
+    ON `ezp_difficulty`.`difficulty`=`ezp_component`.`difficulty`
+  JOIN `ezp_estimate`
+    ON `ezp_estimate`.`difficulty`=`ezp_difficulty`.`difficulty`
+   AND `ezp_estimate`.`devtype`=`ezp_component`.`devtype`
+  WHERE 1
+  GROUP BY `ezp_component`.`package`
+  ORDER BY `ezp_component`.`package`
+  ;
+END$$
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `ezpUnassigned`$$
+CREATE PROCEDURE `ezpUnassigned`(
+  IN        `app` VARCHAR(64) CHARSET ascii
+)
+BEGIN
+  SELECT
+    `ezp_component`.`vendor`
+   ,`ezp_component`.`package`
+   ,`ezp_component`.`handle`
+   ,`ezp_component`.`devtype`
+   ,`ezp_difficulty`.`difficulty` AS `Difficulty_Level`
+   ,`ezp_estimate`.`hours` AS `Estimated_Hours`
+   ,ROUND(`ezp_estimate`.`hours`*((100-`ezp_component`.`progress_pct`)/100),1) AS `Hours_Remaining`
+   ,`ezp_component`.`required_by`
+   ,`ezp_component`.`notes`
+  FROM `ezp_component`
+  JOIN `ezp_difficulty`
+    ON `ezp_difficulty`.`difficulty`=`ezp_component`.`difficulty`
+  JOIN `ezp_estimate`
+    ON `ezp_estimate`.`difficulty`=`ezp_difficulty`.`difficulty`
+   AND `ezp_estimate`.`devtype`=`ezp_component`.`devtype`
+  WHERE `ezp_component`.`package` LIKE CONCAT(app,'-%')
+    AND `ezp_component`.`developer` IS NULL
+  ORDER BY `ezp_component`.`devtype`,`ezp_component`.`handle`
+  ;
+END$$
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `ezpUnscheduled`$$
+CREATE PROCEDURE `ezpUnscheduled`(
+  IN        `app` VARCHAR(64) CHARSET ascii
+)
+BEGIN
+  SELECT
+    `ezp_component`.`vendor`
+   ,`ezp_component`.`package`
+   ,`ezp_component`.`handle`
+   ,`ezp_component`.`devtype`
+   ,`ezp_difficulty`.`difficulty` AS `Difficulty_Level`
+   ,`ezp_estimate`.`hours` AS `Estimated_Hours`
+   ,ROUND(`ezp_estimate`.`hours`*((100-`ezp_component`.`progress_pct`)/100),1) AS `Hours_Remaining`
+   ,`ezp_component`.`developer`
+   ,`ezp_component`.`notes`
+  FROM `ezp_component`
+  JOIN `ezp_difficulty`
+    ON `ezp_difficulty`.`difficulty`=`ezp_component`.`difficulty`
+  JOIN `ezp_estimate`
+    ON `ezp_estimate`.`difficulty`=`ezp_difficulty`.`difficulty`
+   AND `ezp_estimate`.`devtype`=`ezp_component`.`devtype`
+  WHERE `ezp_component`.`package` LIKE CONCAT(app,'-%')
+    AND `ezp_component`.`required_by` IS NULL
+  ORDER BY `ezp_component`.`devtype`,`ezp_component`.`handle`
+  ;
+END$$
+
+DELIMITER ;
+
+-- 2020-01-02 17:09:09
---
+++ _hpapi/EzProject.tables.sql
@@ -0,0 +1,107 @@
+-- Adminer 4.7.5 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+
+CREATE TABLE IF NOT EXISTS `ezp_component` (
+  `vendor` varchar(64) CHARACTER SET ascii NOT NULL,
+  `package` varchar(64) CHARACTER SET ascii NOT NULL,
+  `handle` varchar(64) CHARACTER SET ascii NOT NULL,
+  `devtype` varchar(64) CHARACTER SET ascii NOT NULL,
+  `difficulty` int(1) unsigned NOT NULL DEFAULT '3',
+  `required_by` date DEFAULT NULL,
+  `developer` varchar(64) CHARACTER SET ascii DEFAULT NULL,
+  `progress_pct` int(4) unsigned NOT NULL DEFAULT '0',
+  `notes` text NOT NULL,
+  PRIMARY KEY (`vendor`,`package`,`handle`),
+  KEY `developer` (`developer`),
+  KEY `vendor` (`vendor`),
+  KEY `devtype` (`devtype`),
+  KEY `difficulty` (`difficulty`),
+  KEY `package` (`vendor`,`package`),
+  CONSTRAINT `ezp_component_ibfk_1` FOREIGN KEY (`devtype`) REFERENCES `ezp_devtype` (`devtype`),
+  CONSTRAINT `ezp_component_ibfk_2` FOREIGN KEY (`difficulty`) REFERENCES `ezp_difficulty` (`difficulty`),
+  CONSTRAINT `ezp_component_ibfk_3` FOREIGN KEY (`vendor`, `package`) REFERENCES `ezp_package` (`vendor`, `package`),
+  CONSTRAINT `ezp_component_ibfk_4` FOREIGN KEY (`developer`) REFERENCES `ezp_developer` (`developer`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `ezp_developer` (
+  `developer` varchar(64) CHARACTER SET ascii NOT NULL,
+  PRIMARY KEY (`developer`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `ezp_devtype` (
+  `devtype` varchar(64) CHARACTER SET ascii NOT NULL,
+  `notes` text NOT NULL,
+  `skills` text NOT NULL,
+  PRIMARY KEY (`devtype`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `ezp_difficulty` (
+  `difficulty` int(1) unsigned NOT NULL,
+  `notes` text NOT NULL,
+  PRIMARY KEY (`difficulty`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `ezp_estimate` (
+  `devtype` varchar(64) CHARACTER SET ascii NOT NULL,
+  `difficulty` int(1) unsigned NOT NULL,
+  `hours` int(4) unsigned NOT NULL,
+  PRIMARY KEY (`devtype`,`difficulty`),
+  KEY `estimate_Difficulty` (`difficulty`),
+  CONSTRAINT `ezp_estimate_ibfk_1` FOREIGN KEY (`difficulty`) REFERENCES `ezp_difficulty` (`difficulty`),
+  CONSTRAINT `ezp_estimate_ibfk_2` FOREIGN KEY (`devtype`) REFERENCES `ezp_devtype` (`devtype`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `ezp_package` (
+  `vendor` varchar(64) CHARACTER SET ascii NOT NULL,
+  `package` varchar(64) CHARACTER SET ascii NOT NULL,
+  `notes` text NOT NULL,
+  PRIMARY KEY (`vendor`,`package`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `ezp_project` (
+  `project` varchar(64) CHARACTER SET ascii NOT NULL,
+  `projtype` varchar(64) CHARACTER SET ascii NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`project`),
+  KEY `projtype` (`projtype`),
+  CONSTRAINT `ezp_project_ibfk_1` FOREIGN KEY (`projtype`) REFERENCES `ezp_projtype` (`projtype`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `ezp_projtype` (
+  `projtype` varchar(64) CHARACTER SET ascii NOT NULL,
+  `notes` text NOT NULL,
+  PRIMARY KEY (`projtype`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `ezp_timesheet` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `day` date NOT NULL,
+  `hours` decimal(3,1) unsigned NOT NULL,
+  `developer` varchar(64) CHARACTER SET ascii NOT NULL,
+  `project` varchar(64) CHARACTER SET ascii NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  `vendor` varchar(64) CHARACTER SET ascii DEFAULT NULL,
+  `package` varchar(64) CHARACTER SET ascii DEFAULT NULL,
+  `handle` varchar(64) CHARACTER SET ascii DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `developer` (`developer`),
+  KEY `component` (`vendor`,`package`,`handle`),
+  KEY `project` (`project`),
+  CONSTRAINT `ezp_timesheet_ibfk_2` FOREIGN KEY (`developer`) REFERENCES `ezp_developer` (`developer`),
+  CONSTRAINT `ezp_timesheet_ibfk_3` FOREIGN KEY (`vendor`, `package`, `handle`) REFERENCES `ezp_component` (`vendor`, `package`, `handle`),
+  CONSTRAINT `ezp_timesheet_ibfk_4` FOREIGN KEY (`project`) REFERENCES `ezp_project` (`project`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- 2020-01-02 17:05:47
